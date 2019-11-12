bb44910c67407bd45980efdc01dbef13d3cd05b3
anas <anasbelahrach1994@gmail.com>
2019-11-12 09:19:16 +0100

    add update and delete logic

--- AppController/GameIdeaController.js
+++ AppController/GameIdeaController.js
@@ -54,3 +54,93 @@ if(err) {
       
       });
 }
+module.exports.AddGamesIdea=function(req,res){
+  jwt.verify(req.token, 'configSecret', (err, authData) => {
+    const gamesidea = {
+      Title : req.body.Title,
+      Description :req.body.Description,
+      IdUser:authData.ID
+    }
+if(err) {
+  res.sendStatus(403);
+} else {
+  connection.query('INSERT INTO gamesidea SET ?',[gamesidea], function (error, results, fields) {
+    if (error) {
+      res.json({
+          status:false,
+          message:'there are some error with query'
+      })
+      console.log(error)
+    }else{
+        res.json({
+          status:true,
+          data:results,
+          message:'Post has been added succesfully'
+      })
+      
+    }
+  });
+}
+    
+    });
+}
+
+module.exports.UpdateBookIdea=function(req,res){
+  const gamesidea = {
+    Title : req.body.Title,
+    Description :req.body.Description,
+   
+  }
+  jwt.verify(req.token, 'configSecret', (err, authData) => {
+    // console.log(authData)
+    if(err) {
+      res.sendStatus(403);
+    } else {
+      var query = "UPDATE gamesidea SET Title = ?, Description = ? WHERE ID=?";
+      connection.query(query,[gamesidea.Title,gamesidea.Description,req.params.id], function (error, results, fields) {
+        if (error) {
+          res.json({
+              status:false,
+              message:'there are some error with query'
+          })
+          console.log(error)
+        }else{
+            res.json({
+              status:true,
+              data:results,
+              message:'Post has been updated succesfully'
+          })
+        }
+      });
+    }
+  });
+}
+
+
+
+
+module.exports.DeleteBookIdea=function(req,res){
+
+  jwt.verify(req.token, 'configSecret', (err, authData) => {
+    if(err) {
+      res.sendStatus(403);
+    } else {
+      var query = "DELETE FROM gamesidea WHERE id=?";
+      connection.query(query,[req.params.id], function (error, results, fields) {
+        if (error) {
+          res.json({
+              status:false,
+              message:'there are some error with query'
+          })
+          console.log(error)
+        }else{
+            res.json({
+              status:true,
+              data:results,
+              message:'Post has been delete succesfully'
+          })
+        }
+      });
+    }
+  });
+}
--- Routes/routes.js
+++ Routes/routes.js
@@ -7,4 +7,6 @@ router.post('/register',authController.register);
 router.post('/login',authController.login);
 router.get('/GamesIdea',middleware.middleware,GameIdeaController.showGamesIdea);
 router.post('/GamesIdea/add',middleware.middleware,GameIdeaController.AddGamesIdea);
+router.put('/GamesIdea/edit/:id',middleware.middleware,GameIdeaController.UpdateBookIdea);
+router.delete('/GamesIdea/delete/:id',middleware.middleware,GameIdeaController.DeleteBookIdea);
 module.exports = router;
